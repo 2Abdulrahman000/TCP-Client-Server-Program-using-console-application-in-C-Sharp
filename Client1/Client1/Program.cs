ceba7f9910414b043e31aec9e45c13c93349b6bc
2Abdulrahman000 <115274666+2Abdulrahman000@users.noreply.github.com>
2023-08-10 23:11:45 +0300

    Add files via upload

---
+++ Client1/Client1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client Side");
+            TcpClient client = new TcpClient("192.168.1.6", 1234);
+            try
+            {
+                Console.WriteLine("enter your message");
+                string sdata = Console.ReadLine();
+                int bcount = Encoding.ASCII.GetByteCount(sdata);
+                byte[] bdata = new byte[bcount];
+                bdata= Encoding.ASCII.GetBytes(sdata);
+
+                NetworkStream nstream = client.GetStream();
+                //nstream.Write(bdata, 0, bdata.Length);
+                client.GetStream().Write(bdata, 0, bdata.Length);
+                Console.WriteLine("Send data to server");
+
+                nstream.Close();
+                client.Close();
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed To Connect");
+            }
+        }
+    }
+}
---
+++ Server1/Server1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server side");
+            TcpListener listener = new TcpListener(IPAddress.Any, 1234);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Waiting For Connecting");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Connect is Successed");
+                NetworkStream nstream = client.GetStream();
+                try
+                {
+                    byte[] bdata=new byte[1024];
+                    nstream.Read(bdata,0,bdata.Length);
+                    int recv=0;
+                    foreach (byte b in bdata)
+                    {
+                        if (b != 0) recv++;
+                    }
+                    string sdata = Encoding.ASCII.GetString(bdata,0,recv);
+                    Console.WriteLine("the Client Send : "+sdata);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Connect is Failed");
+                }
+            
+            }
+        }
+    }
+}
